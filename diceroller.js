cca698459cb1a3f4b33a4f8edf1699c5e41077cc
OriginalDad <Renxaisrocks@gmail.com>
2022-10-25 22:01:02 -0400

    minor fixes

--- diceroller.js
+++ diceroller.js
@@ -7,21 +7,6 @@ var nineCount = 0;
 var eightCount = 0;
 var sevenCount = 0;
 
-//code for the dice trick accordion
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
 
 function rollDice() {
 	return Math.floor(Math.random() * 10) + 1;
